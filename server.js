24a4f8a9ce64b43b571e8983f0135be833ef3ff3
Arshavir Azarfar <47248123+Arshazar@users.noreply.github.com>
2020-01-01 02:37:37 +0330

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cors());
 
 app.get('/', (req, res)=> { res.send("bugger it...") })
 app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { /* register.handleRegister(req, res, db, bcrypt) */ res.json(req)})
+app.post('/register', (req, res) => { /* register.handleRegister(req, res, db, bcrypt) */ console.log(req)})
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
